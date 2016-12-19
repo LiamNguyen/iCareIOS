1d542bead048a06e1fb0b098287fcfc1da27623f
LiamNguyen <Phuc.Nguyen2@metropolia.fi>
2016-12-19 18:07:21 +0200

    Starting Contact Info Page

--- DrQMuller/DrQMuller/LoginViewController.swift
+++ DrQMuller/DrQMuller/LoginViewController.swift
@@ -8,86 +8,213 @@
 
 import UIKit
 
-extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int) {
-        assert(red >= 0 && red <= 255, "Invalid red component")
-        assert(green >= 0 && green <= 255, "Invalid green component")
-        assert(blue >= 0 && blue <= 255, "Invalid blue component")
-        
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-    }
-    
-    convenience init(netHex:Int) {
-        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
-    }
-}
+//=========EXTENSION CUSTOM COLOR=========
+
+//extension UIColor {
+//    convenience init(red: Int, green: Int, blue: Int) {
+//        assert(red >= 0 && red <= 255, "Invalid red component")
+//        assert(green >= 0 && green <= 255, "Invalid green component")
+//        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+//        
+//        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+//    }
+//    
+//    convenience init(netHex:Int) {
+//        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+//    }
+//}
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var TopNavBar: UINavigationBar!
-    @IBOutlet weak var txt_Username: UITextField!
-    @IBOutlet weak var txt_Password: UITextField!
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var txtView: UIView!
+    @IBOutlet weak var txt_Username: CustomTxtField!
+    @IBOutlet weak var txt_Password: CustomTxtField!
+    @IBOutlet weak var btn_ResetPassword: UIButton!
     @IBOutlet weak var btn_Login: UIButton!
     @IBOutlet weak var btn_Register: UIButton!
-
+    @IBOutlet weak var constraint_BtnLogin_TxtConfirm: NSLayoutConstraint!
+    
+    var initialViewOrigin: CGFloat!
+    var initialTxtOrigin: CGFloat!
+    var screenHeight: CGFloat!
+    var isIphone4 = false
+    var initialConstraintConstant: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Delegating txtField
+        //=========TXTFIELD DELEGATE=========
         
         self.txt_Username.delegate = self
         self.txt_Password.delegate = self
         
-        //Delegating txtField
+        //=========STYLE FOR RESET PASSWORD LINK=========
         
+        let attrString = NSAttributedString(string: "Quên mật khẩu hoặc tên đăng nhập?", attributes: [NSForegroundColorAttributeName:UIColor.white, NSUnderlineStyleAttributeName: 1])
         
-        btn_Login.layer.cornerRadius = 20
-        btn_Register.layer.cornerRadius = 20
+        btn_ResetPassword.setAttributedTitle(attrString, for: .normal)
         
-        txt_Username.addTarget(self, action: #selector(UITextFieldDelegate.textFieldDidBeginEditing(_:)), for: UIControlEvents.editingChanged)
+        //=========STYLE BUTTONS=========
         
-        txt_Username.addTarget(self, action: #selector(UITextFieldDelegate.textFieldDidEndEditing(_:)), for: UIControlEvents.editingChanged)
+        btn_Login.layer.cornerRadius = 10
+        btn_Register.layer.cornerRadius = 10
+        
+        //=========ASSIGN ORIGIN X SUBVIEW=========
+        
+        initialViewOrigin = loginView.frame.origin.y
+        
+        //=========ASSIGN ORIGIN X VIEW CONTAINS TEXT FIELDS=========
+        
+        initialTxtOrigin = txtView.frame.origin.y
+        
+        //=========ASSIGN CONSTANT CONSTRAINTS BTN_LOGIN_TXT_CONFIRM=========
 
-        txt_Password.addTarget(self, action: #selector(UITextFieldDelegate.textFieldDidBeginEditing(_:)), for: UIControlEvents.editingChanged)
+        initialConstraintConstant = constraint_BtnLogin_TxtConfirm.constant
         
-        txt_Password.addTarget(self, action: #selector(UITextFieldDelegate.textFieldDidEndEditing(_:)), for: UIControlEvents.editingChanged)
+        //=========GET SCREEN SIZE=========
         
-        print("Blahh")
+        screenHeight = UIScreen.main.bounds.height
+        
+        //=========ASSIGN BOOLEAN VARIABLE FOR ISIPHONE4=========
+        
+        if screenHeight == 480 {
+            isIphone4 = true
+        }
+        
+        //=========UPDATE FOR IPHONE 4S SCREEN ON LOAD=========
+        
+        updateLoadStyleForIphone4()
+        
+        //=========DRAW LINE=========
+
+        let firstPoint = CGPoint(x: 0, y: 480)
+        
+        let secondPoint = CGPoint(x: UIScreen.main.bounds.width, y: 480)
+        
+        addLine(fromPoint: firstPoint, toPoint: secondPoint)
+        
+        let thirdPoint = CGPoint(x: 0, y: 480 - 216)
+        let fourthPoint = CGPoint(x: UIScreen.main.bounds.width, y: 480 - 216)
+        
+        addLine(fromPoint: thirdPoint, toPoint: fourthPoint)
     
-
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    //=========TEXT FIELD FOCUS CALL BACK FUNCTION=========
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-
-        if textField == txt_Username {
-            txt_Username.borderStyle = UITextBorderStyle(rawValue: 3)!
-        } else {
-            txt_Password.borderStyle = UITextBorderStyle(rawValue: 3)!
+        if !isIphone4 {
+            adjustViewOrigin(y: 20)
+            return
         }
+        updateTxtFieldFocusStyleForIphone4()
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-//        if textField == txt_Username {
-//            txt_Username.borderStyle = UITextBorderStyle(rawValue: 0)!
-//        } else {
-//            txt_Password.borderStyle = UITextBorderStyle(rawValue: 0)!
-//        }
-    }
+    //=========OUTSIDE TOUCH CLOSE KEYBOARD=========
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+        if !isIphone4 {
+            adjustViewOrigin(y: initialViewOrigin)
+            return
+        }
+        updateTxtFieldLoseFocusStyleForIphone4()
     }
+    
+    //=========PRESS RETURN CLOSE KEYBOARD=========
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        print("test")
+        if !isIphone4 {
+            adjustViewOrigin(y: initialViewOrigin)
+            return true
+        }
+        updateTxtFieldLoseFocusStyleForIphone4()
+        
         return true
+    }
+    
+    //=========ADJUST VIEW ORIGIN Y=========
+    
+    func adjustViewOrigin(y: CGFloat) {
+        loginView.translatesAutoresizingMaskIntoConstraints = true
+        UIView.animate(withDuration: 0.4) { () -> Void in
+            self.loginView.frame.origin = CGPoint(x: self.loginView.frame.origin.x, y: y)
+        }
+    }
+    
+    //=========ADJUST VIEW CONTAIN TEXT FIELDS ORIGIN Y=========
+    
+    func adjustTxtOrigin(y: CGFloat) {
+        txtView.translatesAutoresizingMaskIntoConstraints = true
+        UIView.animate(withDuration: 0.4) { () -> Void in
+            self.txtView.frame.origin = CGPoint(x: self.txtView.frame.origin.x, y: y)
+        }
+    }
+
+    //=========ADJUST LOGIN BUTTON ORIGIN Y=========
+    
+    func adjustBtnOrigin(y: CGFloat) {
+        btn_Login.translatesAutoresizingMaskIntoConstraints = true
+        UIView.animate(withDuration: 0.4) { () -> Void in
+            self.btn_Login.frame.origin = CGPoint(x: self.btn_Login.frame.origin.x, y: y)
+        }
+    }
+    
+    //=========LOCK ROTATION=========
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+    
+    //=========UPDATE UI FOR IPHONE 4S ON LOAD=========
+
+    func updateLoadStyleForIphone4() {
+        if !isIphone4 {
+            return
+        }
+        adjustViewOrigin(y: 30)
+    }
+    
+    //=========UPDATE UI FOR IPHONE 4 WHEN ON TXTFIELD FOCUS=========
+    
+    func updateTxtFieldFocusStyleForIphone4() {
+        adjustViewOrigin(y: 10)
+        adjustTxtOrigin(y: 60)
+        constraint_BtnLogin_TxtConfirm.constant = 20
+        btn_ResetPassword.isHidden = true
+    }
+    
+    //=========REVERT UPDATE UI FOR IPHONE 4 ON TXTFIELD LOSE FOCUS=========
+
+    func updateTxtFieldLoseFocusStyleForIphone4() {
+        adjustViewOrigin(y: 30)
+        adjustTxtOrigin(y: initialTxtOrigin)
+        constraint_BtnLogin_TxtConfirm.constant = initialConstraintConstant
+        btn_ResetPassword.isHidden = false
+    }
+    
+    //=========CREATE LINE=========
+    
+    func addLine(fromPoint start: CGPoint, toPoint end:CGPoint) {
+        let line = CAShapeLayer()
+        let linePath = UIBezierPath()
+        linePath.move(to: start)
+        linePath.addLine(to: end)
+        line.path = linePath.cgPath
+        line.strokeColor = UIColor.red.cgColor
+        line.lineWidth = 2
+        line.lineJoin = kCALineJoinRound
+        self.view.layer.addSublayer(line)
     }
 }
 
--- DrQMuller/DrQMuller/RegisterViewController.swift
+++ DrQMuller/DrQMuller/RegisterViewController.swift
@@ -13,10 +13,21 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var loginView: UIView!
     @IBOutlet weak var txt_Username: CustomTxtField!
     @IBOutlet weak var txt_Password: CustomTxtField!
-    @IBOutlet weak var btn_ResetPassword: UIButton!
-    @IBOutlet weak var btn_Login: UIButton!
+    @IBOutlet weak var txt_ConfirmPassword: CustomTxtField!
     @IBOutlet weak var btn_Register: UIButton!
+    @IBOutlet weak var txtView: UIView!
+    @IBOutlet weak var btn_Back: UIButton!
+    @IBOutlet weak var constraint_BtnBackWidth: NSLayoutConstraint!
+    @IBOutlet weak var constraint_BtnBackHeight: NSLayoutConstraint!
+    @IBOutlet weak var lbl_Welcome: UILabel!
+    @IBOutlet weak var constrant_TxtView_LoginView: NSLayoutConstraint!
+    
     var initialViewOrigin: CGFloat!
+    var initialTxtOrigin: CGFloat!
+    var initialConstraintConstant: CGFloat!
+    var screenWidth: CGFloat!
+    var screenHeight: CGFloat!
+    var isIphone4 = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,44 +36,121 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         
         self.txt_Username.delegate = self
         self.txt_Password.delegate = self
+        self.txt_ConfirmPassword.delegate = self
         
         //=========ASSIGN ORIGIN X SUBVIEW=========
         
         initialViewOrigin = loginView.frame.origin.y
+        
+        //=========ASSIGN ORIGIN X VIEW CONTAINS TEXT FIELDS=========
+        
+        initialTxtOrigin = txtView.frame.origin.y
+        
+        //=========ASSIGN CONSTANT CONSTRAINTS BTN_LOGIN_TXT_CONFIRM=========
+        
+        initialConstraintConstant = constrant_TxtView_LoginView.constant
 
-        //=========STYLE FOR RESET PASSWORD LINK=========
-        
-        let attrString = NSAttributedString(string: "Quên mật khẩu hoặc tên đăng nhập?", attributes: [NSForegroundColorAttributeName:UIColor.white, NSUnderlineStyleAttributeName: 1])
-        
-        btn_ResetPassword.setAttributedTitle(attrString, for: .normal)
-        
         //=========STYLE BUTTONS=========
-
-        btn_Login.layer.cornerRadius = 10
+        
         btn_Register.layer.cornerRadius = 10
+        
+        //=========GET SCREEN SIZE=========
+        
+        screenHeight = UIScreen.main.bounds.height
+        
+        //=========ASSIGN BOOLEAN VARIABLE FOR ISIPHONE4=========
+        
+        if screenHeight == 480 {
+            isIphone4 = true
+        }
+        
+        //=========UPDATE FOR IPHONE 4S SCREEN ON LOAD=========
+        
+        updateLoadStyleForIphone4()
+        
+        //=========INITIALIZE GESTURE RECOGNIZER=========
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture(gesture:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        //=========DRAW LINE=========
+        
+        let firstPoint = CGPoint(x: 0, y: 480)
+        
+        let secondPoint = CGPoint(x: UIScreen.main.bounds.width, y: 480)
+        
+        addLine(fromPoint: firstPoint, toPoint: secondPoint)
+        
+        let thirdPoint = CGPoint(x: 0, y: 480 - 216)
+        let fourthPoint = CGPoint(x: UIScreen.main.bounds.width, y: 480 - 216)
+        
+        addLine(fromPoint: thirdPoint, toPoint: fourthPoint)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    //=========TEXT FIELD FOCUS CALL BACK FUNCTION=========
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        adjustViewOrigin(y: 50)
+        btn_Back.isHidden = true
+
+        //=========IPHONE 6 AND HIGHER=========
+
+        if screenHeight >= 667 {
+            return
+        }
+        
+        if !isIphone4 {
+            adjustViewOrigin(y: 10)
+            adjustTxtOrigin(y: 60)
+            return
+        }
+        updateTxtFieldFocusStyleForIphone4()
     }
 
     //=========OUTSIDE TOUCH CLOSE KEYBOARD=========
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-        adjustViewOrigin(y: initialViewOrigin)
+        btn_Back.isHidden = false
+
+        //=========IPHONE 6 AND HIGHER=========
+
+        if screenHeight >= 667 {
+            return
+        }
+        
+        if !isIphone4 {
+            adjustViewOrigin(y: initialViewOrigin)
+            adjustTxtOrigin(y: initialTxtOrigin)
+            return
+        }
+        updateTxtFieldLoseFocusStyleForIphone4()
     }
     
     //=========PRESS RETURN CLOSE KEYBOARD=========
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        adjustViewOrigin(y: initialViewOrigin)
+        btn_Back.isHidden = false
+
+        //=========IPHONE 6 AND HIGHER=========
+        
+        if screenHeight >= 667 {
+            return true
+        }
+
+        if !isIphone4 {
+            adjustViewOrigin(y: initialViewOrigin)
+            adjustTxtOrigin(y: initialTxtOrigin)
+            return true
+        }
+        updateTxtFieldLoseFocusStyleForIphone4()
+
         return true
     }
     
@@ -75,6 +163,17 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    //=========ADJUST VIEW CONTAIN TEXT FIELDS ORIGIN Y=========
+    
+    func adjustTxtOrigin(y: CGFloat) {
+        txtView.translatesAutoresizingMaskIntoConstraints = true
+        UIView.animate(withDuration: 0.4) { () -> Void in
+            self.txtView.frame.origin = CGPoint(x: self.txtView.frame.origin.x, y: y)
+        }
+    }
+    
+    //=========LOCK ROTATION=========
+    
     override var shouldAutorotate: Bool {
         return false
     }
@@ -82,6 +181,74 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.portrait
     }
+    
+    //=========UPDATE UI FOR IPHONE 4S ON LOAD=========
+    
+    func updateLoadStyleForIphone4() {
+        if !isIphone4 {
+            return
+        }
+        adjustViewOrigin(y: 0)
+        constraint_BtnBackWidth.constant = 25
+        constraint_BtnBackHeight.constant = 25
+    }
+    
+    //=========UPDATE UI FOR IPHONE 4 WHEN ON TXTFIELD FOCUS=========
+    
+    func updateTxtFieldFocusStyleForIphone4() {
+        adjustViewOrigin(y: 10)
+        adjustTxtOrigin(y: 20)
+        lbl_Welcome.isHidden = true
+        constrant_TxtView_LoginView.constant = 0
+    }
+    
+    //=========REVERT UPDATE UI FOR IPHONE 4 ON TXTFIELD LOSE FOCUS=========
+    
+    func updateTxtFieldLoseFocusStyleForIphone4() {
+        adjustViewOrigin(y: 30)
+        adjustTxtOrigin(y: initialTxtOrigin)
+        lbl_Welcome.isHidden = false
+        constrant_TxtView_LoginView.constant = initialConstraintConstant
+    }
+    
+    //=========CREATE LINE=========
+    
+    func addLine(fromPoint start: CGPoint, toPoint end:CGPoint) {
+        let line = CAShapeLayer()
+        let linePath = UIBezierPath()
+        linePath.move(to: start)
+        linePath.addLine(to: end)
+        line.path = linePath.cgPath
+        line.strokeColor = UIColor.red.cgColor
+        line.lineWidth = 2
+        line.lineJoin = kCALineJoinRound
+        self.view.layer.addSublayer(line)
+    }
+    
+    //=========SWIPE TO GET BACK=========
+
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                print("swipe right")
+                self.performSegue(withIdentifier: "segue_RegisterToLogin", sender: self)
+            default:
+                break
+            }
+        }
+    }
+    
+    @IBAction func btn_Register_OnClick(_ sender: Any) {
+        let registerSuccess = true
+        
+        if (registerSuccess) {
+            self.performSegue(withIdentifier: "segue_RegisterToContactInformation", sender: self)
+        } else {
+            print("Register Failed")
+        }
+    }
+    
 }
 
 
